ca0b866ea1cfcf95ef6580d4abb2b73107062bfd
Reem Ibrahim <backupreemibr@gmail.com>
2025-04-30 14:19:51 +0300

    till popups

--- app/src/main/java/com/example/internlink/LoginActivity.java
+++ app/src/main/java/com/example/internlink/LoginActivity.java
@@ -1,24 +1,57 @@
 package com.example.internlink;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ImageButton facebookButton;
+    ImageButton googleButton;
+    Button loginButton;
+    Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        facebookButton = findViewById(R.id.facebookButton);
+        googleButton = findViewById(R.id.googleButton);
+        loginButton = findViewById(R.id.login_Button);
+        signupButton = findViewById(R.id.signup);
+
+        facebookButton.setOnClickListener(v ->
+                Toast.makeText(this, "Facebook login", Toast.LENGTH_SHORT).show());
+
+        googleButton.setOnClickListener(v ->
+                Toast.makeText(this, "Google login", Toast.LENGTH_SHORT).show());
+
+        loginButton.setOnClickListener(v -> showLoginPopup());
+
+        signupButton.setOnClickListener(v -> showSignupPopup());
+    }
+
+    private void showLoginPopup() {
+        BottomSheetDialog loginDialog = new BottomSheetDialog(this);
+        loginDialog.setContentView(R.layout.login_popup);
+
+        loginDialog.getBehavior().setPeekHeight(
+                getResources().getDisplayMetrics().heightPixels / 2
+        );
+
+        loginDialog.show();
+    }
+
+    private void showSignupPopup() {
+        Dialog signupDialog = new Dialog(this, android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+        signupDialog.setContentView(R.layout.signup_popup);
+        signupDialog.show();
     }
 }
